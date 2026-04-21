f0cd692e5e1dfaf6e625a940ee8ef77800b40683
Adrien Carfantan <adrien.carfantan@etu.univ-tours.fr>
2026-04-21 15:31:21 +0200

    ajout des tests costs

---
+++ tests/test_costs.py
@@ -0,0 +1,56 @@
+import pytest
+from src.optimisation_effectif.costs import (
+    ecart_est_valide, 
+    calculer_cout_ecart, 
+    echange_autorise, 
+    est_effectif_valide)
+from src.optimisation_effectif.models import ProblemeDeploiement
+
+@pytest.fixture
+def probleme_test():
+    """Crée un objet de déploiement type pour les tests."""
+    return ProblemeDeploiement(
+        effectif_initial=10,
+        effectif_final=10,
+        mois=["Janvier", "Février"],
+        besoins={"Janvier": 10, "Février": 20},
+        limite_heures_sup=0.25,
+        cout_changement=160,
+        cout_ecart=200,
+        fraction_echanges_max=0.2,
+        echanges_max_absolu=5,
+    )
+
+
+def test_ecart_est_impossible(probleme_test):
+    """ Vérifie que la fonction renvoie False pour un effectif impossible."""
+    assert ecart_est_valide("Février", 10, probleme_test) is False
+
+def test_calculer_cout_ecart_trop_de_manque_plante(probleme_test):
+    """ Vérifie que le calcul echoue si le manque de personnel dépasse la limite."""
+    with pytest.raises(ValueError, match="dépasse la limite d'heures supplémentaires"):
+        calculer_cout_ecart("Février", 10, probleme_test)
+
+def test_calculer_mois_inconnu_ne_plante_pas(probleme_test):
+    """ Vérifie que si le mois n'existe pas, la fonction ne plante pas. """
+    cout, sur, manq = calculer_cout_ecart("fevrier", 10, probleme_test)
+    assert cout == 0.0
+
+def test_echange_est_impossible(probleme_test):
+    """Vérifie qu'un changement d'effectif trop important est refusé."""
+    assert echange_autorise(10, 15, probleme_test) is False
+
+def test_est_effectif_valide_initial_incorrect(probleme_test):
+    """Vérifie que l'effectif du premier mois doit correspondre à l'effectif initial."""
+    assert est_effectif_valide(0, 15, probleme_test) is False
+
+def test_est_effectif_valide_final_incorrect(probleme_test):
+    """Vérifie que l'effectif du dernier mois doit correspondre à l'effectif final."""
+    assert est_effectif_valide(1, 15, probleme_test) is False
+
+def test_calcul_cout_parfait(probleme_test):
+    """Vérifie que le coût est 0 quand l'effectif correspond exactement au besoin."""
+    cout, sur, manq = calculer_cout_ecart("Janvier", 10, probleme_test)
+    assert cout == 0.0
+    assert sur == 0
+    assert manq == 0
